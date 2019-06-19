67f53278fd7a9c0b891e7673bfa88a7920b115fb
Daniel Deans <dwdeans@ncsu.edu>
2019-06-19 01:11:42 -0400

    test1

--- app/src/main/java/com/example/attention_button/MainActivity.kt
+++ app/src/main/java/com/example/attention_button/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                .setAction("Action", null).show()
         }
     }
 
@@ -27,11 +27,16 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+
+    override fun onPanelClosed(featureId: Int, menu: Menu) {
+        super.onPanelClosed(featureId, menu)
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when(item.itemId) {
+        return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
